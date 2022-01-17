0b12deccd4e6a89a2111c1ecf21f578562fcbc9b
hughharford <hugh.harford@poscoconsulting.com>
2022-01-17 13:36:32 +0000

    Just added new course folder
    Few other minor changes during learning

--- src/LinkedIn/PythonEssentials/Chap03/sequence.py
+++ src/LinkedIn/PythonEssentials/Chap03/sequence.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = [ 1, 2, 3, 4, 5 ]
+x = ( 1, 2, 3, 4, 5 )
+# error, as tuple: x[3] = 42
 for i in x:
     print('i is {}'.format(i))
